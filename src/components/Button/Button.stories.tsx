126646ad794b8b779f237fda3ccc1c7873057dd7
Abhay P <abhayp@carestack.com>
2025-06-24 16:00:39 +0530

    published 1

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from './Button';
-import React from 'react';
 
 const meta: Meta<typeof Button> = {
   title: 'Components/Button',
